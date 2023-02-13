803170106d6469afb2dc1b179a82d388480eeb61
RyanRongY <yarong@ucsd.edu>
2023-02-13 11:19:50 -0800

    junit tests

--- app/src/test/java/edu/ucsd/cse110/projects110/ExampleUnitTest.java
+++ app/src/test/java/edu/ucsd/cse110/projects110/ExampleUnitTest.java
@@ -2,7 +2,8 @@ package edu.ucsd.cse110.projects110;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -11,7 +12,28 @@ import static org.junit.Assert.*;
  */
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void testIsValidDouble() {
+        String validDouble = "123.456";
+        String invalidDouble = "abc";
+
+        assertTrue(ValidLocations.isValidDouble(validDouble));
+        assertFalse(ValidLocations.isValidDouble(invalidDouble));
+    }
+
+    @Test
+    public void testIsValidLong() {
+        String validLong = "100.0\u00B0";
+        String invalidLong = "200.0\u00B0";
+
+        assertTrue(ValidLocations.isValidLong(validLong));
+        assertFalse(ValidLocations.isValidLong(invalidLong));
+    }
+    @Test
+    public void testIsValidLat() {
+        String validLat = "90.0\u00B0";
+        String invalidLat = "100.0\u00B0";
+
+        assertTrue(ValidLocations.isValidLat(validLat));
+        assertFalse(ValidLocations.isValidLat(invalidLat));
     }
 }
