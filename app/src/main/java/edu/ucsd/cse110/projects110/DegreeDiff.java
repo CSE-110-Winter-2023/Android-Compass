0265ec1cb78d0f82271c0eb92025450a51837db8
RyanRongY <yarong@ucsd.edu>
2023-02-19 04:05:43 -0800

    close#4,close#2,close#9

--- app/src/main/java/edu/ucsd/cse110/projects110/DegreeDiff.java
+++ app/src/main/java/edu/ucsd/cse110/projects110/DegreeDiff.java
@@ -24,6 +24,21 @@ public class DegreeDiff {
 
         return (float) angle;
     }
+    //mock method to test the same algorithm
+    public static float MockCalculateAngle(double lat1, double lon1, double lat2, double lon2) {
+
+        double dLon = (lon2 - lon1);
+
+        double y = Math.sin(Math.toRadians(dLon)) * Math.cos(Math.toRadians(lat2));
+        double x = Math.cos(Math.toRadians(lat1)) * Math.sin(Math.toRadians(lat2))
+                - Math.sin(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) * Math.cos(Math.toRadians(dLon));
+
+        double angle = Math.atan2(y, x);
+        angle = Math.toDegrees(angle);
+        angle = (angle + 360) % 360;
+
+        return (float) angle;
+    }
 }
 
 
